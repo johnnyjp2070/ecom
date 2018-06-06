77146e3b22944eb2629a5de711150197ed52002f
Johnson <johnnyjp2070@gmail.com>
2018-06-06 17:15:07 +0530

    Added route for Products Page

--- index.js
+++ index.js
@@ -5,15 +5,44 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
-    res.render('home.ejs')
+    res.render('home.ejs', {
+        title: 'HOME'
+    })
 })
 
 app.get('/about', (req, res)=>{
-    res.render('about.ejs')
+    res.render('about.ejs', {
+        title: 'ABOUT'
+    })
+})
+app.get('/products', (req, res)=>{
+    res.render('products.ejs',{
+        title: "Women's Collection"
+    }) 
 })
 app.get('/api', (req, res)=>{
     res.json({
         name: 'Johnson'
     })
+})
+app.get('/product/:id', (req, res)=>{
+    var data = [
+        {
+            id: 1,
+            productName: "Chudithar"
+        },
+        {
+            id: 2,
+            productName: "Salwar"
+        }
+    ]
+    var result;
+    data.forEach((product) => {
+        if(product.id == req.params.id){
+            result = product
+            return true
+        }
+    });
+    console.log(result)
 })
 app.listen(3000)
